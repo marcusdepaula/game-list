8263099c33ba2531be26a4fb915e551064f875ce
Marcus de Paula <mvsp.dev@gmail.com>
2025-08-18 18:20:58 -0300

    Project created

---
+++ src/main/java/com/game_list/gamelist_backend/GamelistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.game_list.gamelist_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelistBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game_list/gamelist_backend/GamelistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game_list.gamelist_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
