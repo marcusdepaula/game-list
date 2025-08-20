0e01d62cf572bdfcc0e9328926d89ae8afc9c5b9
Marcus de Paula <mvsp.dev@gmail.com>
2025-08-20 19:26:38 -0300

    Get game by id

--- src/main/java/com/gamelist/backend/services/GameService.java
+++ src/main/java/com/gamelist/backend/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.gamelist.backend.dto.GameDTO;
 import com.gamelist.backend.dto.GameMinDTO;
 import com.gamelist.backend.entities.Game;
 import com.gamelist.backend.repositories.GameRepository;
@@ -14,7 +16,16 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	
+	//ideal seria ter tratamento de excecao para casos de nao existir o id
+	//acid
+	@Transactional(readOnly = true)
+	public GameDTO findByID(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
 
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
