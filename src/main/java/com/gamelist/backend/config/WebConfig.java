0855b7d9073a2936bb118e08bfc3fa703ada0115
Marcus de Paula <mvsp.dev@gmail.com>
2025-08-22 15:43:28 -0300

    CORS config

--- src/main/java/com/gamelist/backend/config/WebConfig.java
+++ src/main/java/com/gamelist/backend/config/WebConfig.java
@@ -8,19 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-	
-		@Value("${cors.origins}")
-		private String corsOrigins;
 
-		@Bean
-		public WebMvcConfigurer corsConfigurer() {
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
-				}
-			};
-		}
+	@Value("${cors.origins}")
+	private String corsOrigins;
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+			}
+		};
 	}
+
 }
